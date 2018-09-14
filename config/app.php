fd07bfddf23b2e132717130282c164f2d7baa770
Palamax <stephanevalery81@gmail.com>
2018-09-14 15:59:33 +0200

    Signed-off-by: Palamax <stephanevalery81@gmail.com>

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Musculation '.app()->version().' Boilerplate'),
+    'name' => env('APP_NAME', 'Musculation '.app()->version().' Gestion'),
 
     /*
     |--------------------------------------------------------------------------
